46c12b68b9406842600338d5b7b3497369fdad92
sanchitasutar <sanchitasutar24@gmail.com>
2026-03-20 22:55:08 +0530

    Create Vowel_Count.py

---
+++ Week1/Vowel_Count.py
@@ -0,0 +1,15 @@
+string = input("Enter a string: ").lower()
+
+vowels = "aeiou"
+vowel_count = 0
+consonant_count = 0
+
+for char in string:
+    if char.isalpha():
+        if char in vowels:
+            vowel_count += 1
+        else:
+            consonant_count += 1
+
+print("Vowels:", vowel_count)
+print("Consonants:", consonant_count)
